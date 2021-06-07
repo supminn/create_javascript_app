db7c54c3beb117bfef553fda3a94be180d869ce0
supminn <30731236+supminn@users.noreply.github.com>
2021-06-07 20:02:41 +0530

    css loader, optimization, dev setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import _ from "lodash"
+import "./App.css";
+import Logo from "./logo.svg";
+
 const App = ({ name }) => {
   return `
-    <h2> Welcome, ${name} </h2>`;
+  <img src="${Logo}" alt="builder icon"/>
+  <h2> ${_.join(["Welcome,", name], " ")} </h2>
+  <p> Creating vanilla JS applications with modern tooling.</p>`
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const webpackConfig = {
   entry: path.resolve(__dirname, "src", "index.js"),
 
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].bundle.js",
+    path: path.resolve(__dirname, "public"),
+    clean: true,
   },
 
   module: {
@@ -20,7 +22,40 @@ const webpackConfig = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset",
+      },
     ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "JavaScript App",
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        node_vendors: {
+          name: "vendor",
+          test: /[\\/]node_modules[\\/]/,
+          chunks: "all",
+          priority: 1,
+        },
+      },
+    },
+  },
+
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./public",
   },
 
   mode: "production",
