5a013ae0252be643aa49dc9c1f045d273864bd0b
supminn <30731236+supminn@users.noreply.github.com>
2021-06-07 18:48:45 +0530

    babel presets updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
 const path = require("path");
 
 const webpackConfig = {
-    entry: path.resolve(__dirname, "src", "index.js"),
+  entry: path.resolve(__dirname, "src", "index.js"),
 
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 
-    mode: "production"
-}
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+
+  mode: "production",
+};
 
 module.exports = webpackConfig;
